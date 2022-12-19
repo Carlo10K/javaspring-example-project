265a5d58770fe464b28e02ab193b3c2f57e39b39
Carlo10K <carlosortegadiaz50@gmail.com>
2022-12-19 14:14:25 -0600

    [UPDATE]preparing jwt

---
+++ src/main/java/com/example/market/web/security/JWTUtil.java
@@ -0,0 +1,17 @@
+package com.example.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+
+    private static final String KEY = "ba28eb3ded9afefab4a7cbbd6ba1d5cfc970dd1afa5db01a846835978d981f2d";
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *10))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+}
